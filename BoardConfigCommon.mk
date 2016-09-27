1b51765fe50a7da2676eef06c0ea9a96a7409885
kondors1995 <normandija1945@gmail.com>
2016-09-27 13:09:08 +0300

    Adapted from dianlujitao for y550

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,200 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+VENDOR_PATH := device/huawei/msm8916-common
+
+TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8916
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := msm8916
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_CPU_ABI  := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+ENABLE_CPUSETS := true
+
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
+
+# Audio
+AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
+AUDIO_FEATURE_ENABLED_NEW_SAMPLE_RATE := true
+BOARD_USES_ALSA_AUDIO := true
+USE_CUSTOM_AUDIO_POLICY := 1
+USE_XML_AUDIO_POLICY_CONF := 1
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(VENDOR_PATH)/bluetooth
+
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+BOARD_USES_LEGACY_MMAP := true
+TARGET_USE_VENDOR_CAMERA_EXT := true
+BOARD_GLOBAL_CFLAGS += -DCAMERA_VENDOR_L_COMPAT
+
+# Charger
+BOARD_CHARGER_DISABLE_INIT_BLANK := true
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# CMHW
+BOARD_HARDWARE_CLASS := \
+    hardware/cyanogen/cmhw \
+    $(VENDOR_PATH)/cmhw
+TARGET_TAP_TO_WAKE_NODE := "/sys/touch_screen/easy_wakeup_gesture"
+
+# Encryption
+TARGET_HW_DISK_ENCRYPTION := true
+
+# Flags
+BOARD_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+
+# FM
+AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+TARGET_QCOM_NO_FM_FIRMWARE := true
+
+# GPS
+TARGET_NO_RPC := true
+USE_DEVICE_SPECIFIC_GPS := true
+
+# Graphics
+TARGET_USES_OVERLAY := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_HARDWARE_3D := false
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+MAX_EGL_CACHE_SIZE := 2048*1024
+TARGET_USES_ION := true
+TARGET_USES_NEW_ION_API :=true
+TARGET_CONTINUOUS_SPLASH_ENABLED := true
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+TARGET_USES_C2D_COMPOSITION := true
+USE_OPENGL_RENDERER := true
+TARGET_USE_COMPAT_GRALLOC_ALIGN := true
+
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_cherry
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+TARGET_RECOVERY_DEVICE_MODULES := libinit_cherry
+TARGET_UNIFIED_DEVICE := true
+
+# Kernel
+BOARD_KERNEL_BASE        := 0x80000000
+BOARD_KERNEL_PAGESIZE    := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_DTBTOOL_ARGS := -2
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/SMxU-6.0/bin/arm-eabi-
+TARGET_KERNEL_SOURCE := kernel/huawei/msm8916
+TARGET_KERNEL_CONFIG := g620s_defconfig
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Malloc
+MALLOC_SVELTE := true
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1342177280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1795144704
+BOARD_CACHEIMAGE_PARTITION_SIZE := 201326592
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+
+# Properties
+TARGET_SYSTEM_PROP := $(VENDOR_PATH)/system.prop
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
+# Recovery
+TARGET_RECOVERY_DEVICE_DIRS += $(VENDOR_PATH)
+#RECOVERY_VARIANT := twrp
+ifneq ($(RECOVERY_VARIANT),twrp)
+TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/recovery/recovery.fstab
+else
+TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/recovery/twrp.fstab
+RECOVERY_GRAPHICS_FORCE_USE_LINELENGTH := true
+DEVICE_RESOLUTION := 720x1280
+RECOVERY_SDCARD_ON_DATA := true
+TW_NEW_ION_HEAP := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_TARGET_USES_QCOM_BSP := true
+TW_EXTRA_LANGUAGES := true
+TW_INPUT_BLACKLIST := "accelerometer\x0alis3dh-accel"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+endif
+
+# RIL
+TARGET_RIL_VARIANT := caf
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    $(VENDOR_PATH)/sepolicy
+
+# Sensors
+USE_SENSOR_MULTI_HAL := true
+
+# Time services
+BOARD_USES_QC_TIME_SERVICES := true
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HAS_QCOM_WLAN_SDK := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+TARGET_USES_WCNSS_CTRL := true
+TARGET_USES_QCOM_WCNSS_QMI := true
+TARGET_PROVIDES_WCNSS_QMI := true
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# inherit from the proprietary version
+-include vendor/huawei/msm8916-common/BoardConfigVendor.mk
---
+++ init/init_cherry.cpp
@@ -0,0 +1,146 @@
+/*
+   Copyright (c) 2014, The Linux Foundation. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstdlib>
+#include <unistd.h>
+#include <fstream>
+#include <string>
+
+#include <cutils/properties.h>
+#include "vendor_init.h"
+#include "log.h"
+#include "util.h"
+
+#define ISMATCH(a,b)    (!strncmp(a,b,PROP_VALUE_MAX))
+
+void vendor_load_properties()
+{
+    char platform[PROP_VALUE_MAX];
+    std::ifstream fin;
+    std::string buf;
+    int rc;
+
+    rc = property_get("ro.board.platform", platform, NULL);
+    if (!rc || !ISMATCH(platform, ANDROID_TARGET))
+        return;
+
+    fin.open("/proc/app_info", std::ios::in);
+    if (!fin)
+        return;
+
+    while (getline(fin, buf))
+        if (buf.find("huawei_fac_product_name") != std::string::npos)
+            break;
+    fin.close();
+
+    /* C8817D */
+    if (buf.find("C8817D") != std::string::npos) {
+        property_set("ro.product.model", "C8817D");
+        property_set("ro.product.device", "C8817D");
+        property_set("ro.build.product", "C8817D");
+        property_set("persist.radio.multisim.config", "dsds");
+    }
+    /* C8817E */
+    else if (buf.find("C8817E") != std::string::npos) {
+        property_set("ro.product.model", "C8817E");
+        property_set("ro.product.device", "C8817E");
+        property_set("ro.build.product", "C8817E");
+    }
+    /* G621-TL00 */
+    else if (buf.find("G621-TL00") != std::string::npos) {
+        property_set("ro.product.model", "G621-TL00");
+        property_set("ro.product.device", "G621-TL00");
+        property_set("ro.build.product", "G621-TL00");
+        property_set("persist.radio.multisim.config", "dsds");
+    }
+    /* G620S-UL00 */
+    else if (buf.find("G620S-UL") != std::string::npos) {
+        property_set("ro.product.model", "G620S-UL00");
+        property_set("ro.product.device", "G620S-UL00");
+        property_set("ro.build.product", "G620S-UL00");
+        property_set("persist.radio.multisim.config", "dsds");
+    }
+    /* G620S-L01 */
+    else if (buf.find("G620S-L01") != std::string::npos) {
+        property_set("ro.product.model", "G620S-L01");
+        property_set("ro.product.device", "G620S-L01");
+        property_set("ro.build.product", "G620S-L01");
+    }
+    /* G620S-L02 */
+    else if (buf.find("G620S-L02") != std::string::npos) {
+        property_set("ro.product.model", "G620S-L02");
+        property_set("ro.product.device", "G620S-L02");
+        property_set("ro.build.product", "G620S-L02");
+    }
+    /* Che1-CL10 */
+    else if (buf.find("Che1-CL10") != std::string::npos) {
+        property_set("ro.product.model", "Che1-CL10");
+        property_set("ro.product.device", "Che1-CL10");
+        property_set("ro.build.product", "Che1-CL10");
+        property_set("persist.radio.multisim.config", "dsds");
+    }
+    /* Che1-CL20 */
+    else if (buf.find("Che1-CL20") != std::string::npos) {
+        property_set("ro.product.model", "Che1-CL20");
+        property_set("ro.product.device", "Che1-CL20");
+        property_set("ro.build.product", "Che1-CL20");
+        property_set("persist.radio.multisim.config", "dsds");
+    }
+    /* Che1-L04 */
+    else if (buf.find("Che1-L04") != std::string::npos) {
+        property_set("ro.product.model", "Che1-L04");
+        property_set("ro.product.device", "Che1-L04");
+        property_set("ro.build.product", "Che1-L04");
+        property_set("persist.radio.multisim.config", "dsds");
+    }
+    /* Y550-L01 */
+    else if (buf.find("Y550-L01") != std::string::npos) {
+        property_set("ro.product.model", "Y550-L01");
+        property_set("ro.product.device", "Y550-L01");
+        property_set("ro.build.product", "Y550-L01");
+        property_set("ro.telephony.default_network", "9");
+        property_set("ro.sf.lcd_density","240");
+    }
+    /* Y550-L02 */
+    else if (buf.find("Y550-L02") != std::string::npos) {
+        property_set("ro.product.model", "Y550-L02");
+        property_set("ro.product.device", "Y550-L02");
+        property_set("ro.build.product", "Y550-L02");
+        property_set("ro.telephony.default_network", "9");
+        property_set("ro.sf.lcd_density","240");
+    }
+    /* Y550-L03 */
+    else if (buf.find("Y550-L03") != std::string::npos) {
+        property_set("ro.product.model", "Y550-L03");
+        property_set("ro.product.device", "Y550-L03");
+        property_set("ro.build.product", "Y550-L03");
+        property_set("ro.telephony.default_network", "9");
+        property_set("ro.sf.lcd_density","240");
+    }
+}
