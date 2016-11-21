LineageOS/android_device_huawei_msm8916-common

msm8916: camera: Add support for manual 3A.
Change-Id: I4907117bb5affc11878b459ac55b1d6e4d520eb4

BoardConfigCommon.mk
@@ -19,6 +19,8 @@
 
 VENDOR_PATH := device/huawei/msm8916-common
 
+TARGET_SPECIFIC_HEADER_PATH := $(PLATFORM_PATH)/include
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno306

kondors1995/android_device_msm8916-common

BoardConfigCommon.mk
@@ -19,6 +19,8 @@
 
 VENDOR_PATH := device/huawei/msm8916-common
 
+TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8916
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno306