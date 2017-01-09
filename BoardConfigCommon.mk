d1759e0cdd2de1c298f10bc35736d36dbdb08f9b
kondors1995 <normandija1945@gmail.com>
2017-01-09 18:40:50 +0200

    soviet
    Change-Id: I31ea25a75a7c3035401542b4182d5fdc821af55f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,8 +115,8 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_DTBTOOL_ARGS := -2
-TARGET_KERNEL_SOURCE := kernel/huawei/msm8916
-TARGET_KERNEL_CONFIG := lineageos_cherry_defconfig
+TARGET_KERNEL_SOURCE := kernel/huawei/Soviet-kernel
+TARGET_KERNEL_CONFIG := g620s_defconfig
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
